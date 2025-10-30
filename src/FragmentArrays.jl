985ff07030fcf068e6ebcd2798b4b0518d40179b
Talom Laël <gesee37@gmail.com>
2025-10-30 09:22:45 +0100

    Made Core files #1

---
+++ src/FragmentArrays.jl
@@ -0,0 +1,16 @@
+#####################################################################################################################
+################################################# FRAGMENT ARRAYS ###################################################
+#####################################################################################################################
+
+"""
+    module FragmentArrays
+
+This module implement fragmenting arrays, whose are arrays that divide themself on deletions
+Kinda like sparse arrays.
+"""
+module FragmentArrays
+
+## Core data structures
+include("core.jl")
+
+end # module
