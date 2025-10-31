da19e4de724ed74a5b54e82c448185bb7802cd23
Talom Laël <gesee37@gmail.com>
2025-10-31 17:16:46 +0100

    Updated insertion

--- src/operations.jl
+++ src/operations.jl
@@ -34,90 +34,151 @@ function Base.append!(f::FragmentVector, v)
 	append!(f.data[id], v)
 end
 
-function Base.insert!(f::FragmentVector, i, v)
-	map = f.map
-	mask = f.map[i]
+@inline function decode_mask(mask::UInt64)
+    blockid = Int(mask >> 32)
+    start   = Int(mask & OFFSET_MASK)
+    return blockid, start
+end
 
-	if iszero(mask)
-		if 1 < i < length(map)
-			ml, mr = map[i-1], map[i+1]
-			cl,cr = iszero(ml), iszero(mr)
+@inline function make_mask(blockid::Int, start::Int)
+    return (UInt64(blockid) << 32) | UInt64(start & Int(OFFSET_MASK))
+end
 
-			left = ml >> 32
-			right = mr >> 32
+function _bump_block_starts!(map::Vector{UInt64}, from_index::Int)
+    
+    @inbounds for k in 1:length(map)
+        m = map[k]
+        if m != 0
+            
+            s = Int(m & OFFSET_MASK)
+            if s >= from_index
+                
+                high = m & ~OFFSET_MASK
+                newlow = UInt64(s + 1) & OFFSET_MASK
+                map[k] = high | newlow
+            end
+        end
+    end
+    return
+end
 
-			if cl && cr
-				blockid = max(_search_index(f.map, i) >> 32, 1)
-			    insert!(f.data, blockid, [v])
-			    map[i] = blockid << 32 | i-1
-			    @inbounds for j in i+1:length(map)
-			 	    map[j] += (1*iszero(map[j])) << 32
-				end
-				
-				return
-			end
+function Base.insert!(f::FragmentVector{T}, i::Int, v::T) where T
+    map = f.map
+    lmap = length(map)
+    @boundscheck 1 <= i <= lmap || throw(BoundsError(f, i))
 
-			if iszero(left)
-				map[i] = mr
-				block = f.data[right]
-				map[i:i+length(block)+1] .+= 1
+    mask = map[i]
+    if mask != 0
+        
+        bid, bstart = decode_mask(mask)
+        local = i - bstart
+        @inbounds begin
+            insert!(f.data[bid], local, v)    
+            _bump_block_starts!(map, i)
+            
+        end
+        return
+    end
 
-				pushfirst!(block, v)
-			elseif iszero(right)
-				map[i] = ml
-				block = f.data[left]
+    left_exists = (i > 1 && map[i-1] != 0)
+    right_exists = (i < lmap && map[i+1] != 0)
 
-				push!(block, v)
-			else
-				bl, br = f.data[left], f.data[right]
-				map[i] = left
-				push!(bl, v)
-				offset = mr & OFFSET_MASK
-				noffset = ml & OFFSET_MASK
+    if left_exists && right_exists
+        
+        lmask = map[i-1]; rmask = map[i+1]
+        lbid, lstart = decode_mask(lmask)
+        rbid, rstart = decode_mask(rmask)
 
-				_fuse_block!(bl, br, map, left, offset, noffset)
-				_deleteat!(f.data, right)
-			end
-		else
-			l = length(map)
+        
+        if lbid == rbid
+            
+            local = i - lstart
+            @inbounds insert!(f.data[lbid], local, v)
+            _bump_block_starts!(map, i)
+            return
+        end
 
-			if l == 1
-				map[l] = 1
-				push!(f.data, [v])
-			end
-			if i == 1
-				map[i] = 1
-				if iszero(map[i+1])
-					pushfirst!(f.data, [v])
-					@inbounds for j in 2:l
-					    map[j] += (1 * !iszero(map[j])) << 32
-					end
-				else
-					pushfirst!(f.data[1], v)
-				end
-			else
-				id = length(f.data)
-				map[i] = id << 32 | i-1
+        
+        left_block = f.data[lbid]
+        right_block = f.data[rbid]
 
-				if iszero(map[i-1])
-					push!(f.data, [v])
-				else
-					push!(f.data[end], v)
-				end
-			end
-		end
-	else
-		id, offset = (mask) >> 32, mask & OFFSET_MASK
-	    dl = length(f.data[id])
-	
-		insert!(f.data[id], i, v)
-		insert!(map, i, id)
-		
-		for i in offset+dl:length(map)
-			map[i] += 1 * iszero(map[i])
-		end
-	end
-end 
+        
+        if i == lstart + length(left_block)
+            
+            push!(left_block, v)
+            
+            map[i] = make_mask(lbid, lstart)
+            _bump_block_starts!(map, i+1)  
+            return
+        elseif i == rstart - 1
+            
+            insert!(right_block, 1, v)
+            map[i] = make_mask(rbid, rstart - 1)
+            _bump_block_starts!(map, i+1)
+            return
+        else
+            
+            nbid = length(f.data) + 1
+            push!(f.data, Vector{T}([v]))
+            map[i] = make_mask(nbid, i - 1)
+            _bump_block_starts!(map, i+1)
+            return
+        end
+    elseif left_exists
+        
+        lmask = map[i-1]
+        lbid, lstart = decode_mask(lmask)
+        left_block = f.data[lbid]
+        if i == lstart + length(left_block)
+            push!(left_block, v)
+            map[i] = make_mask(lbid, lstart)
+            _bump_block_starts!(map, i+1)
+            return
+        else
+            
+            nbid = length(f.data) + 1
+            push!(f.data, Vector{T}([v]))
+            map[i] = make_mask(nbid, i - 1)
+            _bump_block_starts!(map, i+1)
+            return
+        end
+    elseif right_exists
+        
+        rmask = map[i+1]
+        rbid, rstart = decode_mask(rmask)
+        right_block = f.data[rbid]
+        if i == rstart - 1
+            insert!(right_block, 1, v)
+            
+            newstart = rstart - 1
+            @inbounds for k in 1:length(map)
+                m = map[k]
+                if m != 0
+                    b, s = decode_mask(m)
+                    if b == rbid
+                        
+                        map[k] = make_mask(b, newstart)
+                    end
+                end
+            end
+            _bump_block_starts!(map, i+1)
+            return
+        else
+            nbid = length(f.data) + 1
+            push!(f.data, Vector{T}([v]))
+            map[i] = make_mask(nbid, i - 1)
+            _bump_block_starts!(map, i+1)
+            return
+        end
+    else
+        
+        nbid = length(f.data) + 1
+        push!(f.data, Vector{T}([v]))
+        map[i] = make_mask(nbid, i - 1)
+        _bump_block_starts!(map, i+1)
+        return
+    end
+end
 
 function Base.pop!(f::FragmentVector)
 	f.map[end] = 0
